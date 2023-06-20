045ef8aeaba5540df4de915dc73a210735a1a8b7
EstevaoPeixoto <127219555+EstevaoPeixoto@users.noreply.github.com>
2023-06-20 19:14:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ let subMenu = document.getElementById("subMenu");
 function toggleMenu(){
     subMenu.classList.toggle("open-menu");
 }
+
+const searchBtn = document.querySelector(".search-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const searchBox = document.querySelector(".search-box");
+const searchInput = document.querySelector("input");
+searchBtn.onclick = () =>{
+    searchBox.classList.add("active");
+    searchInput.classList.add("active");
+    searchBtn.classList.add("active");
+    cancelBtn.classList.add("active");
+}
+cancelBtn.onclick = () =>{
+    searchBox.classList.remove("active");
+    searchInput.classList.remove("active");
+    searchBtn.classList.remove("active");
+    cancelBtn.classList.remove("active");
+}
